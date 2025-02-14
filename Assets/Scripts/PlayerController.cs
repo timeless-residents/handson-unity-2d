c9926208d5e00be58aa328e91494e843cf5df94a
Timeless Residents <business@satotakuya.jp>
2025-02-15 06:42:28 +0900

    init commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb.linearVelocity = movement * moveSpeed;
+    }
+}
